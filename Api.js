0145b2e8502544cf65cbb7b691bac94e19713300
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-03-10 23:04:34 +0530

    Update Api.js

--- Api.js
+++ Api.js
@@ -1,1 +1,23 @@
+ //working on Node.js-api
+    
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello From other side");
+})
+
+//output display on the webpage
+
+
+app.post("/students",(req,res)=>{
+    res.send("Hello sexy Satyam");
+})
+
+app.listen(port,()=>{
+    console.log(`Connected to this ${port}`)
+})
+//to run the app Node.js-api\Restfulapi> node src/app.js
+
 
